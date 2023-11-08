2fafa1f7940f13e85c9adfdfefed0a3d9ef2c5c1
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2023-11-08 15:23:26 +0100

    added areArgsCorrect function, that checks whether given maps are correct, meaning if each device does not have more components on it than their limit, or whether there are devices with limit = 0

--- cp2023/solution/StorageSystemFactory.java
+++ cp2023/solution/StorageSystemFactory.java
@@ -7,6 +7,7 @@
  */
 package cp2023.solution;
 
+import java.util.HashMap;
 import java.util.Map;
 
 import cp2023.base.ComponentId;
@@ -15,18 +16,46 @@ import cp2023.base.StorageSystem;
 
 
 public final class StorageSystemFactory {
-    private static boolean isDeviceMapCorrect(Map<DeviceId, Integer> deviceTotalSlots)
+    private static void isDeviceMapCorrect(Map<DeviceId, Integer> deviceTotalSlots) throws IllegalArgumentException
     {
-        return true;
+        // we check if any device has 0 capacity, if such device exist we return false
+        for(Integer capacity : deviceTotalSlots.values())
+            if(capacity == 0)
+                throw new IllegalArgumentException();
     }
-    private static boolean isComponentMapCorrect(Map<ComponentId, DeviceId> componentPlacement)
+    private static void areArgumentsCorrect(Map<DeviceId, Integer> deviceTotalSlots,
+                                               Map<ComponentId, DeviceId> componentPlacement) throws IllegalArgumentException
     {
-        return true;
+        isDeviceMapCorrect(deviceTotalSlots);
+
+        Map<DeviceId, Integer> howManyComponentsOnDevices = new HashMap<>();
+
+        for(DeviceId key : deviceTotalSlots.keySet())
+            howManyComponentsOnDevices.put(key, 0);
+
+        // we count how many components is currently on each device
+        for(DeviceId id : componentPlacement.values())
+        {
+            Integer val = howManyComponentsOnDevices.get(id);
+            howManyComponentsOnDevices.put(id, val + 1);
+        }
+        // we check if current number of components on device is <= maxNnbrOfComponents on device
+        for(DeviceId id : deviceTotalSlots.keySet()) {
+            Integer maxNumberOfDevices = deviceTotalSlots.get(id);
+            Integer currentNumberOfDevices = howManyComponentsOnDevices.get(id);
+
+            if (currentNumberOfDevices > maxNumberOfDevices)
+                throw new IllegalArgumentException();
+        }
     }
     public static StorageSystem newSystem(
             Map<DeviceId, Integer> deviceTotalSlots,
-            Map<ComponentId, DeviceId> componentPlacement) {
+            Map<ComponentId, DeviceId> componentPlacement) throws IllegalArgumentException {
+
+        areArgumentsCorrect(deviceTotalSlots, componentPlacement);
+
         return new cp2023.solution.StorageSystem(deviceTotalSlots, componentPlacement);
+
         // FIXME: implement
         //throw new RuntimeException("not implemented");
     }
