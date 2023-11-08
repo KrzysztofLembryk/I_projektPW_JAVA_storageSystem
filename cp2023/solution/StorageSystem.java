0faf8a73ce432b5872ecf0aa16f7fc087d00482e
KrzysztofLembryk <krzysztof.lembryk@gmail.com>
2023-11-08 14:49:14 +0100

    first commit

---
+++ cp2023/solution/StorageSystem.java
@@ -0,0 +1,29 @@
+package cp2023.solution;
+
+import cp2023.base.ComponentId;
+import cp2023.base.ComponentTransfer;
+import cp2023.base.DeviceId;
+import cp2023.exceptions.TransferException;
+
+import java.util.Map;
+
+public class StorageSystem implements cp2023.base.StorageSystem {
+    // deviceTotalSlots - stores info about how many components
+    // device of given ID can store (deviceID --> capacity).
+    private Map<DeviceId, Integer> deviceTotalSlots;
+
+    // componentInDevicePlacement - remembers on which device given
+    // component is stored.
+    private Map<ComponentId, DeviceId> componentInDevicePlacement;
+    public StorageSystem(Map<DeviceId, Integer> deviceTotalSlots,
+                                Map<ComponentId, DeviceId> componentPlacement)
+    {
+        this.deviceTotalSlots = deviceTotalSlots;
+        this.componentInDevicePlacement = componentPlacement;
+    }
+
+    @Override
+    public void execute(ComponentTransfer transfer) throws TransferException {
+
+    }
+}
---
+++ cp2023/solution/StorageSystemFactory.java
@@ -0,0 +1,34 @@
+/*
+ * University of Warsaw
+ * Concurrent Programming Course 2023/2024
+ * Java Assignment
+ *
+ * Author: Konrad Iwanicki (iwanicki@mimuw.edu.pl)
+ */
+package cp2023.solution;
+
+import java.util.Map;
+
+import cp2023.base.ComponentId;
+import cp2023.base.DeviceId;
+import cp2023.base.StorageSystem;
+
+
+public final class StorageSystemFactory {
+    private static boolean isDeviceMapCorrect(Map<DeviceId, Integer> deviceTotalSlots)
+    {
+        return true;
+    }
+    private static boolean isComponentMapCorrect(Map<ComponentId, DeviceId> componentPlacement)
+    {
+        return true;
+    }
+    public static StorageSystem newSystem(
+            Map<DeviceId, Integer> deviceTotalSlots,
+            Map<ComponentId, DeviceId> componentPlacement) {
+        return new cp2023.solution.StorageSystem(deviceTotalSlots, componentPlacement);
+        // FIXME: implement
+        //throw new RuntimeException("not implemented");
+    }
+
+}
